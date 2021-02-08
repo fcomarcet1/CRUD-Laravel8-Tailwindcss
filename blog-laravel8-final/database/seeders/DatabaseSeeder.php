bbcf48493c41d0644d506128424a01b8d9b7aeb9
fcomarcet1 <fcomarcet1@gmail.com>
2021-02-08 12:09:51 +0100

    first commit

---
+++ blog-laravel8-final/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->times(1)->create([
+            "name" => "cursosdesarrolloweb",
+            "email" => "app@cursosdesarrolloweb.es",
+            "password" => bcrypt("password")
+        ]);
+        Project::factory()->times(40)->create();
+    }
+}
